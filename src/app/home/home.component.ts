0e8138a204522939d6a306b8454fabd05ac68bc4
Przemyslaw Rogala <przemorog01@gmail.com>
2024-10-07 23:32:36 +0200

    Filter popup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,16 +2,26 @@ import { Component } from '@angular/core';
 import { CitySearchComponent } from '../city-search/city-search.component';
 import { JobofferlistComponent } from '../jobofferlist/jobofferlist.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [CitySearchComponent, JobofferlistComponent, FormsModule],
+  imports: [CitySearchComponent, JobofferlistComponent, FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   searchKey : string = "";
+  visibility = 'inactive';
 
+
+  showFilters(){
+    this.visibility = 'active';
+  }
+
+  close(){
+    this.visibility = 'inactive';
+  }
 }
